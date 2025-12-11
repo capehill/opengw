5a00aea4d988a27ebf1eff3200219a6d8267c1f2
capehill <juha.niemimaki@gmail.com>
2025-12-11 21:49:19 +0200

    Avoid division by zero

--- profiler.cpp
+++ profiler.cpp
@@ -12,7 +12,7 @@ profiler::~profiler()
 {
     const auto frequency = SDL_GetPerformanceFrequency();
     const auto duration = static_cast<double>(performanceCounter) / static_cast<double>(frequency);
-    const auto average = 1000.0 * duration / static_cast<double>(runCounter);
+    const auto average = runCounter ? 1000.0 * duration / static_cast<double>(runCounter) : 0.0f;
     const auto longestMillis = 1000 * longest / frequency;
     printf("[%s] Duration %f s, run counter %" SDL_PRIu64 ", average %f ms, longest %" SDL_PRIu64 " ms\n",
            name.c_str(), duration, runCounter, average, longestMillis);
